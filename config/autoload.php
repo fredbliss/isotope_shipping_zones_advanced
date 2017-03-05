b81e62eba3a69d1d1f9d2b835341eb00ce4828b2
fbliss <fred.bliss@intelligentspark.com>
2017-03-04 19:54:50 -0500

    corrected path to template file

--- config/autoload.php
+++ config/autoload.php
@@ -24,5 +24,5 @@ if (class_exists('NamespaceClassLoader')) {
  */
 TemplateLoader::addFiles(array
 (
-    'iso_checkout_step_shipping_upgrade'     => 'system/modules/iso_shipping_zones_advanced/templates/checkout'
+    'iso_checkout_step_shipping_upgrade'     => 'system/modules/isotope_shipping_zones_advanced/templates/checkout'
 ));
